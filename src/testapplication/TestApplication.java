20cb25871a353cb11ce540aed76a8dd5ee4e42bd
Stefanos Koutsouflakis <koutsouflakis.stefanos@gmail.com>
2021-03-05 22:55:57 +0200

    mychange

--- src/testapplication/TestApplication.java
+++ src/testapplication/TestApplication.java
@@ -6,7 +6,7 @@ public class TestApplication {
 
    
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello WORLD OF JAVA");
         System.out.println("Here's new Panos");
     }
 
